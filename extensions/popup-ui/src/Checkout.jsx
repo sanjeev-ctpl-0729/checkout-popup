25805d42648e016ee5bf048724874e393eb6abed
sanjeev-ctpl-0729 <sanjeev.k@codilar.com>
2026-02-24 15:09:59 +0530

    modify : wrap special instruction in heading

--- extensions/popup-ui/src/Checkout.jsx
+++ extensions/popup-ui/src/Checkout.jsx
@@ -295,7 +295,7 @@ function Extension() {
           </s-choice-list>
 
           {/* Special instructions */}
-          <s-text>special instructions</s-text>
+          <s-heading>special instructions</s-heading>
           <s-text-area
             name="specialInstructions"
             label="Leave special instructions like business hours, business name etc to ensure a smooth delivery."
